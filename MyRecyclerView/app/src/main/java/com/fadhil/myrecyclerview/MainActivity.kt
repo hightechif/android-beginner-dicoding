c2e9918d40a3126de8058e80bed6ad0730311b94
R. Fadhil <ridhanfadhilah@gmail.com>
2021-07-28 17:57:22 +0700

    RecyclerView Grid Mode

--- MyRecyclerView/app/src/main/java/com/fadhil/myrecyclerview/MainActivity.kt
+++ MyRecyclerView/app/src/main/java/com/fadhil/myrecyclerview/MainActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.fadhil.myrecyclerview.adapter.GridHeroAdapter
+import com.fadhil.myrecyclerview.adapter.ListHeroAdapter
 
 class MainActivity : AppCompatActivity() {
     private lateinit var rvHeroes: RecyclerView
@@ -41,11 +44,22 @@ class MainActivity : AppCompatActivity() {
     private fun setMode(selectedMode: Int) {
         when (selectedMode) {
             R.id.action_list -> {
+                showRecyclerList()
             }
+
             R.id.action_grid -> {
+                showRecyclerGrid()
             }
+
             R.id.action_cardview -> {
+
             }
         }
+    }
+
+    private fun showRecyclerGrid() {
+        rvHeroes.layoutManager = GridLayoutManager(this, 2)
+        val gridHeroAdapter = GridHeroAdapter(list)
+        rvHeroes.adapter = gridHeroAdapter
     }
 }
--- MyRecyclerView/app/src/main/java/com/fadhil/myrecyclerview/adapter/ListHeroAdapter.kt
+++ MyRecyclerView/app/src/main/java/com/fadhil/myrecyclerview/adapter/ListHeroAdapter.kt
@@ -1,4 +1,4 @@
-package com.fadhil.myrecyclerview
+package com.fadhil.myrecyclerview.adapter
 
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +8,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.fadhil.myrecyclerview.Hero
+import com.fadhil.myrecyclerview.R
 
 class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
 
